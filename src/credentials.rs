d3ca5e3f801f3aebc33820ed4a54bc5914253de2
Tomkoid <tomaszierl@outlook.com>
2024-04-26 18:03:21 +0200

    credentials: better error handling
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,3 +1,6 @@
+use log::error;
+
+#[derive(Debug)]
 pub struct Credentials {
     pub homeserver: String,
     pub username: String,
@@ -7,16 +10,20 @@ pub struct Credentials {
 pub fn init_credentials() -> anyhow::Result<Credentials> {
     // get username and password from environment variables
     let homeserver = std::env::var("MATRIX_HOMESERVER").unwrap_or("https://matrix.org".to_string());
-    let username = std::env::var("MATRIX_USERNAME").unwrap_or("".to_string());
-    let password = std::env::var("MATRIX_PASSWORD").unwrap_or("".to_string());
+    let username = std::env::var("MATRIX_USERNAME").unwrap_or(String::new());
+    let password = std::env::var("MATRIX_PASSWORD").unwrap_or(String::new());
 
     if homeserver.is_empty() {
+        error!("Missing matrix homeserver (MATRIX_HOMESERVER)");
+
         return Err(anyhow::anyhow!(
             "Missing matrix homeserver (MATRIX_HOMESERVER)"
         ));
     }
 
     if username.is_empty() || password.is_empty() {
+        error!("Missing username (MATRIX_USERNAME) or password (MATRIX_PASSWORD)");
+
         return Err(anyhow::anyhow!(
             "Missing username (MATRIX_USERNAME) or password (MATRIX_PASSWORD)"
         ));
